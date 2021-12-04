5c1ced3710ab255e21fe15be8e0eeaf1ac9b64e5
lilbillybiscuit <qianbill2014@outlook.com>
2021-12-03 23:00:00 -0500

    Initialize database script

--- init.js
+++ init.js
@@ -3,14 +3,14 @@ var fs = require('fs')
 let rawdata = fs.readFileSync('config.json');
 let config = JSON.parse(rawdata);
 console.log(config);
-const databasename=con.escapeId(config.database.database);
+
 var con = mysql.createConnection({
   host: config.database.host,
   user: config.database.user,
-  password: databasename,
+  password: config.database.password,
   multipleStatements:true
 });
-
+const databasename=con.escapeId(config.database.database);
 con.connect(function(err) {
   if (err) throw err;
 });
@@ -25,5 +25,9 @@ con.query("CREATE DATABASE IF NOT EXISTS "+databasename+";",
   }
 );
 
-con.query(`CREATE TABLE IF NOT EXISTS ${databasename}.users (username VARCHAR(20) PRIMARY KEY NOT NULL, userid INTEGER NOT NULL, passwordsha512 VARCHAR(128) NOT NULL, name VARCHAR(60), data JSON)`);
-con.query(`CREATE TABLE IF NOT EXISTS ${databasename}.announcements (id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT, title VARCHAR(250) NOT NULL, summary VARCHAR(500), data JSON)`);
+con.query(`CREATE TABLE IF NOT EXISTS ${databasename}.users (username VARCHAR(30) PRIMARY KEY, userid INTEGER NOT NULL, passwordsha512 VARCHAR(128) NOT NULL, name VARCHAR(60), data JSON)`);
+con.query(`CREATE TABLE IF NOT EXISTS ${databasename}.announcements (id INTEGER AUTO_INCREMENT PRIMARY KEY  NOT NULL , title VARCHAR(250) NOT NULL, summary VARCHAR(500), dateadded DATETIME, data JSON)`);
+con.query(`CREATE TABLE IF NOT EXISTS ${databasename}.profiles (name VARCHAR(50), data JSON)`);
+con.query(`CREATE TABLE IF NOT EXISTS ${databasename}.calendar (date DATETIME, data JSON)`)
+
+con.end();
